30af6435c4a7e40689767860840c9a47d3495f94
AlysaJarrell <cra20007@byui.edu>
2022-10-11 17:47:50 -0600

    seeker game and other updates

--- Unit03/Game/Seeker.cs
+++ Unit03/Game/Seeker.cs
@@ -3,6 +3,52 @@ using System;
 
 namespace Unit03.Game
 {
+
+        /// <summary>
+        /// <para>The person looking for the Hider.</para>
+        /// <para>
+        /// The responsibility of a Seeker is to keep track of its location.
+        /// </para>
+        /// </summary>
+        public class Seeker
+        {
+            private int _location;
+
+
+        /// <summary>
+        /// Constructs a new instance of Seeker.
+        /// </summary>
+
+        public Seeker()
+        {
+            _location = 0;
+
+        }
+
+        /// <summary>
+        /// Gets the current location.
+        /// </summary>
+        /// <returns>The current location as an int.</returns>
+        public int GetLocation()
+        {
+            return _location;
+        }
+
+        /// <summary>
+        /// Moves to the given location.
+        /// </summary>
+        /// <param name="location">The given location.</param>
+        public void MoveLocation(int location)
+        {
+            _location = location;
+        }
+
+
+
+
+        }
+
+
     // TODO: Implement the Seeker class as follows...
 
     // 1) Add the class declaration. Use the following class comment.
@@ -13,15 +59,11 @@ namespace Unit03.Game
         /// The responsibility of a Seeker is to keep track of its location.
         /// </para>
         /// </summary>
-
-
     // 2) Create the class constructor. Use the following method comment.
         
         /// <summary>
         /// Constructs a new instance of Seeker.
         /// </summary>
-       
-
     // 3) Create the GetLocation() method. Use the following method comment.
         
         /// <summary>
